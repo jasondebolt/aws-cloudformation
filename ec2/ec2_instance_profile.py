2b34668148d846a1dfb088daf13e4e3767b414bb
jasondebolt <jasondebolt@gmail.com>
2017-05-13 13:52:13 -0700

    refactored ec2/* files

--- ec2/ec2_instance_profile.py
+++ ec2/ec2_instance_profile.py
@@ -3,67 +3,88 @@
 Create an IAM Role, IAM Policy, and EC2 Instance Profile for EC2 Instances.
 """
 from troposphere import Ref, Template
+from troposphere.logs import LogGroup
 import troposphere.iam as iam
 
 
-def attach_instance_profile(template):
-    """Attached an IAM Role, IAM Policy, and EC2 Instance Profile to a
-    CloudFormation template and returns the template."
-    """
-    template.add_resource(iam.Role(
-        "EC2Role",
-        AssumeRolePolicyDocument={
-            "Version": "2012-10-17",
-            "Statement": [{
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": ["ec2.amazonaws.com", "opsworks.amazonaws.com"]
-                },
-                "Action": ["sts:AssumeRole"]
-            }]
-        },
-        ManagedPolicyArns=[
-            "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-        ],
-        Path="/"
-    ))
+LOG_POLICY_NAME = 'LogPolicy'
+LOG_GROUP_NAME = 'CloudFormationLogs'
+LOG_RETENTION_DAYS = 7
 
-    # Inline policy for the given role defined in the Roles attribute.
-    template.add_resource(iam.PolicyType(
-        "LogPolicy",
-        PolicyName="LogPolicy",
-        PolicyDocument={
-            "Version": "2012-10-17",
-            "Statement": [{
-                "Effect": "Allow",
-                "Resource": [
-                    "*"
-                ],
-                "Action": [
-                    "logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents",
-                    "logs:DescribeLogStreams"
-                ]
-            }]
-        },
-        Roles=[Ref("EC2Role")]
-    ))
 
-    template.add_resource(iam.InstanceProfile(
-        "EC2RoleInstanceProfile",
-        Path="/",
-        Roles=[Ref("EC2Role")]
-    ))
-    return template
+class Ec2Resources(object):
+    """Cloudformation resources that EC2 instances depend on."""
+    def __init__(self, template):
+        self.template = template
+
+    def attach_ec2_instance_profile(self):
+        """Attached an IAM Role, IAM Policy, and EC2 Instance Profile to a
+        CloudFormation template and returns the template."
+        """
+        self.template.add_resource(iam.Role(
+            "EC2Role",
+            AssumeRolePolicyDocument={
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": ["ec2.amazonaws.com",
+                                    "opsworks.amazonaws.com"]
+                    },
+                    "Action": ["sts:AssumeRole"]
+                }]
+            },
+            ManagedPolicyArns=[
+                "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+            ],
+            Path="/"
+        ))
+
+        # Inline policy for the given role defined in the Roles attribute.
+        self.template.add_resource(iam.PolicyType(
+            LOG_POLICY_NAME,
+            PolicyName=LOG_POLICY_NAME,
+            PolicyDocument={
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Effect": "Allow",
+                    "Resource": [
+                        "*"
+                    ],
+                    "Action": [
+                        "logs:CreateLogGroup",
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents",
+                        "logs:DescribeLogStreams"
+                    ]
+                }]
+            },
+            Roles=[Ref("EC2Role")]
+        ))
+
+        self.template.add_resource(iam.InstanceProfile(
+            "EC2RoleInstanceProfile",
+            Path="/",
+            Roles=[Ref("EC2Role")]
+        ))
+
+        self.template.add_resource(LogGroup(
+            LOG_GROUP_NAME,
+            RetentionInDays=LOG_RETENTION_DAYS
+        ))
+
+        return self.template
 
 
 def main():
+    """Generates a cloudformation template."""
     template = Template()
     template.add_version('2010-09-09')
     template.add_description('Create an IAM Role, IAM Policy, and EC2 '
-                             'Instance Profile for EC2 Instances.')
-    template = attach_instance_profile(template)
+                             'Instance Profile, and a CloudFormation log group '
+                             'for EC2 Instances.')
+    ec2_resources = Ec2Resources(template)
+    template = ec2_resources.attach_ec2_instance_profile()
     print template.to_json()
 
 
--- ec2/new_chef_node.py
+++ ec2/new_chef_node.py
@@ -1,89 +1,217 @@
+"""Generates an AWS CloudFormation template to bootstrap a Chef node."""
 from troposphere import Base64, FindInMap, Join, Output, GetAtt, Tags
-from troposphere import Parameter, Ref, Template
+from troposphere import Ref, Template
 from troposphere import cloudformation
 import troposphere.ec2 as ec2
-import ec2_header
+import ec2_parameters # pylint: disable=W0403
+import ec2_instance_profile # pylint: disable=W0403
 
+EC2_INSTANCE = 'Ec2Instance'
 
-t = Template()
-t.add_version("2010-09-09")
-t.add_description("Create an EC2 instance which automatically bootstraps a Chef node using Chef unattended installs.")
-t = ec2_header.setEc2Header(t)
+def attach_chef_node(template):
+    """Attaches a bootstrapped Chef Node EC2 instance to an
+    AWS CloudFormation template and returns the template.
+    """
+    template = ec2_parameters.attach_ec2_parameters(template)
+    template = ec2_instance_profile.attach_ec2_instance_profile(template)
 
-security_group = t.add_resource(ec2.SecurityGroup(
-    'SecurityGroup',
-    GroupDescription='Allows SSH access from anywhere',
-    SecurityGroupIngress=[
-        ec2.SecurityGroupRule(
-            IpProtocol='tcp',
-            FromPort=22,
-            ToPort=22,
-            CidrIp=Ref(t.parameters['SSHLocation'])
-        )
-    ],
-    Tags=Tags(
-        Name='ChefNodeSecurityGroup'
-    )
-))
-
-ec2_instance = t.add_resource(ec2.Instance(
-    'Ec2Instance',
-    ImageId=FindInMap("AWSRegionArch2AMI", Ref("AWS::Region"), FindInMap("AWSInstanceType2Arch", Ref(t.parameters['InstanceType']), "Arch")),
-    InstanceType=Ref(t.parameters['InstanceType']),
-    KeyName=Ref(t.parameters['KeyName']),
-    SecurityGroups=[Ref(security_group)],
-    IamInstanceProfile='LogRoleInstanceProfile',
-    UserData=Base64(Join('', [
-        '#!/bin/bash\n',
-        'sudo apt-get update\n',
-        'sudo apt-get -y install python-setuptools\n',
-        'sudo apt-get -y install python-pip\n',
-        'sudo pip install https://s3.amazonaws.com/cloudformation-examples/',
-        'aws-cfn-bootstrap-latest.tar.gz\n',
-        'cfn-init -s \'', Ref('AWS::StackName'),
-        '\' -r Ec2Instance -c ascending'
-    ])),
-    Metadata=cloudformation.Metadata(
-        cloudformation.Init(
-            cloudformation.InitConfigSets(
-                ascending=['config1', 'config2'],
-                descending=['config2', 'config1']
-            ),
-            config1=cloudformation.InitConfig(
-                commands={
-                    'test': {
-                        'command': 'echo "$CFNTEST" > text.txt',
-                        'env': {
-                            'CFNTEST': 'I come from config1.'
-                        },
-                        'cwd': '~'
-                    }
-                }
-            ),
-            config2=cloudformation.InitConfig(
-                commands={
-                    'test': {
-                        'command': 'echo "$CFNTEST" > text.txt',
-                        'env': {
-                            'CFNTEST': 'I come from config2.'
-                        },
-                        'cwd': '~'
-                    }
-                }
+    security_group = template.add_resource(ec2.SecurityGroup(
+        'SecurityGroup',
+        GroupDescription='Allows SSH access from anywhere',
+        SecurityGroupIngress=[
+            ec2.SecurityGroupRule(
+                IpProtocol='tcp',
+                FromPort=22,
+                ToPort=22,
+                CidrIp=Ref(template.parameters['SSHLocation'])
             )
+        ],
+        Tags=Tags(
+            Name='ChefNodeSecurityGroup'
         )
-    ),
-    Tags=Tags(
-        Name='ops.cfninit',
-        env='ops'
-    )
-))
+    ))
 
-t.add_output(Output(
-    'PublicIp',
-    Description='Public IP of the newly created EC2 instance',
-    Value=GetAtt(ec2_instance, 'PublicIp')
-))
+    ec2_instance = template.add_resource(ec2.Instance(
+        EC2_INSTANCE,
+        ImageId=FindInMap(
+            "AWSRegionArch2AMI", Ref("AWS::Region"),
+            FindInMap("AWSInstanceType2Arch",
+                      Ref(template.parameters['InstanceType']), "Arch")),
+        InstanceType=Ref(template.parameters['InstanceType']),
+        KeyName=Ref(template.parameters['KeyName']),
+        SecurityGroups=[Ref(security_group)],
+        IamInstanceProfile=Ref(template.resources['EC2RoleInstanceProfile']),
+        UserData=Base64(Join('', [
+            '#!/bin/bash\n', 'sudo apt-get update\n',
+            'sudo apt-get -y install python-setuptools\n',
+            'sudo apt-get -y install python-pip\n',
+            ('sudo pip install https://s3.amazonaws.com'
+             '/cloudformation-examples/'),
+            'aws-cfn-bootstrap-latest.tar.gz\n',
+            '\n',
+            '/opt/aws/bin/cfn-init -v ',
+            '         --stack ', Ref('AWS::StackName'),
+            '         --resource {0} '.format(EC2_INSTANCE),
+            '         --configsets InstallAndRun ',
+            '         --region ', Ref('AWS::Region'),
+            '\n'
+        ])),
+        Metadata=cloudformation.Metadata(
+            cloudformation.Init(
+                cloudformation.InitConfigSets(
+                    InstallAndRun=['Install', 'InstallLogs', 'Configure']
+                ),
+                Install=cloudformation.InitConfig(
+                    packages={
+                        'yum': {
+                            'stress': [],
+                            'docker': []
+                        }
+                    },
+                    files={
+                        '/etc/cfn/cfn-hup.conf': {
+                            'content': Join('', [
+                                '[main]\n',
+                                'stack=', Ref('AWS::StackId'), '\n',
+                                'region=', Ref('AWS::Region'), '\n',
+                                'interval=1', '\n'
+                                ]),
+                            'mode': '000400',
+                            'owner': 'root',
+                            'group': 'root'
+                        },
+                        '/etc/cfn/hooks.d/cfn-auto-reloader.conf': {
+                            'content': Join('', [
+                                '[cfn-auto-reloader-hook]\n',
+                                'triggers=post.update\n',
+                                ('path=Resources.{0}.Metadata.'
+                                 'AWS::CloudFormation::Init\n'
+                                 .format(EC2_INSTANCE)),
+                                'action=/opt/aws/bin/cfn-init -v ',
+                                '       --stack ', Ref('AWS::StackName'),
+                                '       --resource {0} '.format(EC2_INSTANCE),
+                                '       --configsets InstallAndRun ',
+                                '       --region ', Ref('AWS::Region'), '\n',
+                                'runas=root\n'
+                            ]),
+                        }
+                    },
+                    services={
+                        'sysvinit': {
+                            'docker': {
+                                'enabled': 'true',
+                                'ensureRunning': 'true'
+                            },
+                            'cfn-hup': {
+                                'enabled': 'true',
+                                'ensureRunning': 'true'
+                            }
+                        }
+                    },
+                    commands={
+                        'test': {
+                            'command': 'echo "$CFNTEST" > Install.txt',
+                            'env': {
+                                'CFNTEST': 'I come from Install.'
+                            },
+                            'cwd': '~'
+                        }
+                    }
+                ),
+                InstallLogs=cloudformation.InitConfig(
+                    packages={
+                        'yum': {
+                            'awslogs': []
+                        }
+                    },
+                    files={
+                        '/etc/awslogs/awslogs.conf': {
+                            'content': Join('', [
+                                '[general]\n',
+                                'state_file= /var/awslogs/state/agent-state\n',
+
+                                '[/var/log/cloud-init.log]\n',
+                                'file = /var/log/cloud-init.log\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/cloud-init.log\n',
+                                'datetime_format = \n',
+
+                                '[/var/log/cloud-init-output.log]\n',
+                                'file = /var/log/cloud-init-output.log\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/cloud-init-output.log\n',
+                                'datetime_format = \n',
+
+                                '[/var/log/cfn-init.log]\n',
+                                'file = /var/log/cfn-init.log\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/cfn-init.log\n',
+                                'datetime_format = \n',
+
+                                '[/var/log/cfn-hup.log]\n',
+                                'file = /var/log/cfn-hup.log\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/cfn-hup.log\n',
+                                'datetime_format = \n',
+
+                                '[/var/log/cfn-wire.log]\n',
+                                'file = /var/log/cfn-wire.log\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/cfn-wire.log\n',
+                                'datetime_format = \n',
+
+                                '[/var/log/httpd]\n',
+                                'file = /var/log/httpd/*\n',
+                                'log_group_name = ', { 'Ref': 'CloudFormationLogs' }, '\n',
+                                'log_stream_name = {instance_id}/httpd\n',
+                                'datetime_format = %d/%b/%Y:%H:%M:%S\n'
+                            ])
+                        }
+
+                    },
+                    commands={
+                        'test': {
+                            'command': 'echo "$CFNTEST" > InstallLogs.txt',
+                            'env': {
+                                'CFNTEST': 'I come from install_logs.'
+                            },
+                            'cwd': '~'
+                        }
+                    }
+                ),
+                Configure=cloudformation.InitConfig(
+                    commands={
+                        'test': {
+                            'command': 'echo "$CFNTEST" > Configure.txt',
+                            'env': {
+                                'CFNTEST': 'I come from Configure.'
+                            },
+                            'cwd': '~'
+                        }
+                    }
+                )
+            )
+        ),
+        Tags=Tags(
+            Name='ops.cfninit',
+            env='ops'
+        )
+    ))
+
+    template.add_output(Output(
+        'PublicIp',
+        Description='Public IP of the newly created EC2 instance',
+        Value=GetAtt(ec2_instance, 'PublicIp')
+    ))
+    return template
 
 
-print(t.to_json())
+def main():
+    template = Template()
+    template.add_version('2010-09-09')
+    template.add_description('Create a fully bootstrapped Chef node.')
+    template = attach_chef_node(template)
+    print template.to_json()
+
+if __name__ == '__main__':
+    main()
